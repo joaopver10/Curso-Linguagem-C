d840692aea05830a86cedef195eab1cdaea5b4bc
João Pedro <joaovitorino@unigranrio.br>
2021-03-10 22:12:18 -0300

    aula ponteiro

--- Aula17/aula17.cpp
+++ Aula17/aula17.cpp
@@ -6,4 +6,19 @@ using namespace std;
 
 int main(){
 
+    int a = 20;
+    printf("A variavel tem valor: %d \n", a);
+    printf("o ponteiro esta no local da memoria de numero: %d  \n", &a);
+
+    // ponteiro
+    int *ponteiro;
+
+    ponteiro = &a;
+
+    printf("o ponteiro e: %d  \n", ponteiro);
+
+    *ponteiro = 10;
+
+    printf("o a agora vale %d  \n", a);
+    return 0;
 }
